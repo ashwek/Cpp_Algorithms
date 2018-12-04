6329e44277416d556fbf19c409b750eb597485c6
ashwek <ashwekswamy@protonmail.com>
2018-12-04 20:18:27 +0530

    Update Stack.cpp

--- 3_Data_Structures/10_Elementary_Data_Structures/Stack.cpp
+++ 3_Data_Structures/10_Elementary_Data_Structures/Stack.cpp
@@ -1,67 +1,74 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
 class Stack{
 
+	private:
+		int *data, top, size;
+
 	public:
+
 		Stack(int s = 10){
 			data = new int[s];
 			top = -1;
 			size = s;
 		}
+
 		int is_empty(){
 			return (top == -1);
 		}
+
 		int is_full(){
 			return (top == size-1);
 		}
-		void push(){
-			if(is_full())
-				cout<<"\n\tSatck Overflow";
-			else{
-				cout<<"\tEnter Value = ";
-				cin>>data[++top];
+
+		int push(int value){
+			if( is_full() )
+				return 0;
+			data[++top] = value;
+			return 1;
+		}
+
+		int pop(){
+			if( is_empty() )
+				return -1;
+			return data[top--];
+		}
+
+		void display(){
+			if( ! is_empty() ){
+				for(int i=top; i>=0; i--) cout<<data[i] <<", ";
 			}
 		}
-		void pop(){
-			if(is_empty())
-				cout<<"\n\tSatck Underflow";
-			else
-				cout<<"\n\tDeleted Value = " <<data[top--];
-		}
-		void display(){
-			if(is_empty())
-				cout<<"\n\tSatck is empty";
-			else
-				cout<<"\n\tStack : ";
-				for(int i=top; i>=0; i--) cout<<data[i] <<", ";
-		}
-	private:
-		int *data, top, size;
 };
 
 
 int main(){
 
-	Stack one = Stack(3);
-	cout<<"\n\tPush 4 Elements : \n";
-	one.push();
-	one.push();
-	one.push();
-	one.push();
-	one.display();
-	
-	cout<<"\n\n\tPop 1 Elements : ";
-	one.pop();
-	one.display();
-	
-	cout<<"\n\n\tPop 3 Elements : ";
-	one.pop();
-	one.pop();
-	one.pop();
-	
+	Stack S1 = Stack(5);
+	int i = 1;
+	while( ! S1.is_full() ){
+		cout<<"Push " <<i <<endl;
+		S1.push(i);
+		i += 5;
+	}
+
+	cout<<"\n\nStack : ";
+	S1.display();
+
+	cout<<"\n\nPop, popped value = " <<S1.pop();
+
+	cout<<"\n\nStack : ";
+	S1.display();
 	cout<<endl;
-	
+
+	while( ! S1.is_empty() ){
+		cout<<"Popped value " <<S1.pop() <<endl;
+	}
+
+	cout<<endl;
+
 	return 0;
 }

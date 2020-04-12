597d566a05f5cc0ea54371becc4c834bf119afb1
ashwek <ashwekswamy@protonmail.com>
2020-04-13 00:13:23 +0530

    Rewrite Max_SubArray_Problem.cpp

--- 1_Foundations/4_Divide_And_Conquer/Max_SubArray_Problem.cpp
+++ 1_Foundations/4_Divide_And_Conquer/Max_SubArray_Problem.cpp
@@ -1,68 +1,74 @@
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
-void Max_Across(int Arr[], int s, int m, int e, int &temp_s, int &temp_e, int &temp_sum){
-	
-	temp_s = m-1, temp_e = m;
-	int L_Sum = Arr[temp_s], R_Sum = Arr[temp_e];
-	int Sum = 0;
-	
-	for(int i=m-1; i>=s; i--){
-		Sum+=Arr[i];
-		if(Sum > L_Sum){
-			L_Sum = Sum;
-			temp_s = i;
-		}
-	}
-	Sum = 0;
-	for(int i=m; i<e; i++){
-		Sum+=Arr[i];
-		if(Sum > R_Sum){
-			R_Sum = Sum;
-			temp_e = i;
-		}
-	}
-	temp_sum = L_Sum + R_Sum;
+void max_across(int arr[], int s, int m, int e, int &temp_s, int &temp_e, int &temp_sum){
+
+    temp_s = m - 1, temp_e = m;
+    int leftSum = arr[temp_s], rightSum = arr[temp_e];
+    int sum = 0;
+
+    for(int i = m - 1; i >= s; i--){
+        sum += arr[i];
+        if( sum > leftSum ){
+            leftSum = sum;
+            temp_s = i;
+        }
+    }
+
+    sum = 0;
+    for(int i = m; i < e; i++){
+        sum += arr[i];
+        if( sum > rightSum){
+            rightSum = sum;
+            temp_e = i;
+        }
+    }
+
+    temp_sum = leftSum + rightSum;
+
 }
 
-void Max_Sub_Array(int Arr[], int s, int e, int &Left_Index, int &Right_Index, int &Sum){
-	
-	if(e-s == 1){
-		Left_Index = s;
-		Right_Index = e;
-		Sum = Arr[s];
-		return ;
-	}
-	
-	int m = (e+s)/2;
-	int temp_s, temp_e, temp_sum;
-	
-	Max_Sub_Array(Arr, s, m, Left_Index, Right_Index, Sum);
-	Max_Sub_Array(Arr, m, e, temp_s, temp_e, temp_sum);
-	
-	if(temp_sum > Sum){
-		Left_Index = temp_s;
-		Right_Index = temp_e;
-		Sum = temp_sum;
-	}
-	
-	Max_Across(Arr, s, m, e, temp_s, temp_e, temp_sum);
-	
-	if(temp_sum > Sum){
-		Left_Index = temp_s;
-		Right_Index = temp_e;
-		Sum = temp_sum;
-	}
-	
+void max_sub_array(int arr[], int s, int e, int &left_index, int &right_index, int &sum){
+
+    if( e - s == 1 ){
+        left_index = s;
+        right_index = e;
+        sum = arr[s];
+        return ;
+    }
+
+    int m = (e + s) / 2;
+    int temp_s, temp_e, temp_sum;
+
+    max_sub_array(arr, s, m, left_index, right_index, sum);
+    max_sub_array(arr, m, e, temp_s, temp_e, temp_sum);
+
+    if( temp_sum > sum ){
+        left_index = temp_s;
+        right_index = temp_e;
+        sum = temp_sum;
+    }
+
+    max_across(arr, s, m, e, temp_s, temp_e, temp_sum);
+
+    if(temp_sum > sum){
+        left_index = temp_s;
+        right_index = temp_e;
+        sum = temp_sum;
+    }
+
 }
 
 int main(){
 
-	int Arr[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7}, n = 16;
-	
-	int Left_Index, Right_Index, Sum;
-	Max_Sub_Array(Arr, 0, n, Left_Index, Right_Index, Sum);
-	cout<<Left_Index <<" " <<Right_Index <<" " <<Sum <<endl;
-	
-	return 0;
+    int arr[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+    int n = 16;
+
+    int left_index, right_index, sum;
+    max_sub_array(arr, 0, n, left_index, right_index, sum);
+    cout<<left_index <<" " <<right_index <<" " <<sum <<endl;
+
+    return 0;
 }

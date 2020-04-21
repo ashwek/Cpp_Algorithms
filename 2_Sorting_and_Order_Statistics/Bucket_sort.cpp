51f92ecae77a0fd9f004264846249607dd9cdc2f
ashwek <ashwekswamy@protonmail.com>
2020-04-22 00:16:05 +0530

    Update Bucket_Sort.cpp

--- 2_Sorting_and_Order_Statistics/Bucket_sort.cpp
+++ 2_Sorting_and_Order_Statistics/Bucket_sort.cpp
@@ -1,52 +1,78 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdlib.h>
+#include <cmath>
 
 using namespace std;
 
-void Print(int Arr[], int n){
-	for(int i=0; i<n; i++) cout<<Arr[i] <<", ";
+int get_first_digit(int num, int len){
+
+    return num / pow(10, len);
 }
 
-int get_digit(int num, int digit){
-	int temp;
-	while(num > 0){
-		temp = num%10;
-		num /= 10;
-		digit--;
-	}
-	return (digit == 0) ? temp : 0;
+int max(int arr[], int n){
+
+    int m = arr[0];
+    for(int i = 1; i < n; i++){
+        if( arr[i] > m )
+            m = arr[i];
+    }
+    return m;
+
 }
 
-void bucket_sort(int Arr[], int n){
+void bucket_sort(int arr[], int n){
 
-	vector<int> bucket[10];
+    vector<int> bucket[10];
 
-	for(int i=0; i<n; i++){
-		bucket[get_digit(Arr[i], 3)].push_back(Arr[i]);
-	}
+    int len = to_string(max(arr, n)).size();
 
-	int k = 0;
-	for(int i=0; i<10; i++){
-		sort(bucket[i].begin(), bucket[i].end());
-		for(int j=0; j<bucket[i].size(); j++){
-			Arr[k++] = bucket[i][j];
-		}
-	}
+    for(int i = 0; i < n; i++) {
+        bucket[get_first_digit(arr[i], len)].push_back(arr[i]);
+    }
+
+    int k = 0;
+    for(int i = 0; i < 10; i++){
+        sort(bucket[i].begin(), bucket[i].end());
+        for(int j = 0; j < bucket[i].size(); j++) {
+            arr[k++] = bucket[i][j];
+        }
+    }
+
 }
 
 int main(){
 
-	int Arr[] = {352, 154, 242, 427, 174, 323, 66, 90, 289, 4}, n = 10;
+    // seed random
+    srand(time(NULL));
 
-	cout<<"Orignal Array = ";
-	Print(Arr, n);
+    // get random size (bettwen 1 & 10000)
+    int n = random() % 10000 + 1;
+    int arr[n];
 
-	bucket_sort(Arr, n);
+    // initialize random array
+    cout <<"Size = " <<n <<"\n\nOrignal Array : ";
+    for(int i = 0; i < n; i++){
+        arr[i] = random() % 1000;
+        cout <<arr[i] <<", ";
+    }
 
-	cout<<"\n Sorted Array = ";
-	Print(Arr, n);
+    clock_t start, end;
+    start = clock();
 
-	cout<<endl;
-	return 0;
+    bucket_sort(arr, n);
+
+    end = clock();
+
+    cout<<"\n\nSorted Array = ";
+    for(int i = 0; i < n; i++)
+        cout <<arr[i] <<", ";
+
+    cout <<"\n\nSorted " <<n <<" elements in "
+        <<((float)(end - start)) / CLOCKS_PER_SEC <<" seconds";
+
+    cout<<endl;
+    return 0;
+
 }

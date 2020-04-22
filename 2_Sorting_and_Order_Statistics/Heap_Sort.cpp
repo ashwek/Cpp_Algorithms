4990b772f4c42e183a6ed13389cb15258e3439d0
ashwek <ashwekswamy@protonmail.com>
2020-04-22 12:33:21 +0530

    Update Heap_Sort.cpp

--- 2_Sorting_and_Order_Statistics/Heap_Sort.cpp
+++ 2_Sorting_and_Order_Statistics/Heap_Sort.cpp
@@ -1,58 +1,88 @@
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
-#define Child(i, pos)  2*i+pos
+#define child(i, pos)  2 * i + pos
 
-void Max_Heapify(int Heap[], int i, int Len){
+template<class T>
+void max_heapify(T arr[], int i, int len){
 
-	int Left_Child = Child(i, 1);		//1 to indicate Left Child
-	int Right_Child = Child(i, 2);		//2 to indicate Right Child
-	int temp, Largest = i;
-	
-	//Find the largest element among the Root, Left child and Right child
-	if(Left_Child < Len and Heap[Left_Child] > Heap[i]) Largest = Left_Child;
-	if(Right_Child < Len and Heap[Right_Child] > Heap[Largest]) Largest = Right_Child;
-	
-	//if root is not the largest, Swap root with largest and Heapify again
-	if(Largest != i){
-		temp = Heap[Largest];
-		Heap[Largest] = Heap[i];
-		Heap[i] = temp;
-		Max_Heapify(Heap, Largest, Len);
-	}
-	
-}
+    int left_child = child(i, 1);		//1 to indicate Left Child
+    int right_child = child(i, 2);		//2 to indicate Right Child
+    int largest = i;
+    T temp;
 
-void Build_Max_Heap(int Heap[], int N){
-	
-	for(int i=(N/2)-1; i>=0; i--)
-		Max_Heapify(Heap, i, N);
+    //Find the largest element among the Root, Left child and Right child
+    if(left_child < len && arr[left_child] > arr[i])
+        largest = left_child;
+    if(right_child < len && arr[right_child] > arr[largest])
+        largest = right_child;
+
+    //if root is not the largest, Swap root with largest and Heapify again
+    if(largest != i) {
+        temp = arr[largest];
+        arr[largest] = arr[i];
+        arr[i] = temp;
+        max_heapify(arr, largest, len);
+    }
 
 }
 
-void Print(int Heap[], int N){
-	cout<<"\n\tHeap : ";
-	for(int i=0; i<N; i++) cout<<Heap[i] <<", ";
+template<class T>
+void build_max_heap(T arr[], int n) {
+
+    for(int i = (n / 2) - 1; i >= 0; i--)
+        max_heapify(arr, i, n);
+
 }
 
-void Heap_Sort(int Heap[], int N){
+template<class T>
+void heap_sort(T arr[], int n) {
 
-	int temp;
-	
-	Build_Max_Heap(Heap, N);		//Build Heap
-	for(int i=0; i<N; i++){
-		temp = Heap[0];		//Swap Root Of Heap with the last element
-		Heap[0] = Heap[N-i-1];
-		Heap[N-i-1] = temp;
-		Max_Heapify(Heap, 0, N-i-1);  //Heapify again from root, reduce length to ignore the last (sorted) elements in array
-	}
+    T temp;
+
+    build_max_heap(arr, n);		//Build Heap
+    for(int i = 0; i < n; i++) {
+        temp = arr[0];		//Swap Root Of Heap with the last element
+        arr[0] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+        max_heapify(arr, 0, n - i - 1);  //Heapify again from root, reduce length to ignore the last (sorted) elements in array
+    }
+
 }
 
 int main(){
-	
-	int Heap[] = {13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7}, N=16;
-	Print(Heap, N);		//Print Orignal Array
-	Heap_Sort(Heap, N);		//Heap Sort
-	Print(Heap, N);		//Print Sorted Array
-	cout<<endl;	
+
+    // seed random
+    srand(time(NULL));
+
+    // get random size (bettwen 1 & 10000)
+    int n = random() % 10000 + 1;
+    int arr[n];
+
+    // initialize random array
+    cout <<"Size = " <<n <<"\n\nOrignal Array : ";
+    for(int i = 0; i < n; i++){
+        arr[i] = random() % 1000;
+        cout <<arr[i] <<", ";
+    }
+
+    clock_t start, end;
+    start = clock();
+
+    heap_sort(arr, n);
+
+    end = clock();
+
+    cout<<"\n\nSorted Array = ";
+    for(int i = 0; i < n; i++)
+        cout <<arr[i] <<", ";
+
+    cout <<"\n\nSorted " <<n <<" elements in "
+        <<((float)(end - start)) / CLOCKS_PER_SEC <<" seconds";
+
+    cout<<endl;
+    return 0;
+
 }

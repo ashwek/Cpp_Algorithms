4901587f0450ce326a892340c25910d771415396
ashwek <ashwekswamy@protonmail.com>
2020-04-22 18:49:55 +0530

    use template class in Binary_Search_Tree.cpp

--- 3_Data_Structures/12_Binary_Search_Tree/Binary_Search_Tree.cpp
+++ 3_Data_Structures/12_Binary_Search_Tree/Binary_Search_Tree.cpp
@@ -16,157 +16,165 @@ https://en.wikipedia.org/wiki/Binary_search_tree
 
 using namespace std;
 
+template<class DataType>
 class Node{
-    public:
-        Node *parent, *left, *right;
-        int value;
+public:
+    Node<DataType> *parent, *left, *right;
+    DataType value;
 
-        Node(Node *Parent = NULL, int Value = 0, Node *Left = NULL, Node *Right = NULL){
-            parent = Parent;
-            value = Value;
-            left = Left;
-            right = Right;
-        }
+    Node(Node<DataType> *parent, DataType value,
+        Node<DataType> *left = NULL, Node<DataType> *right = NULL) {
+        this->parent = parent;
+        this->value = value;
+        this->left = left;
+        this->right = right;
+    }
+
 };
 
+template<class DataType>
 class BST{
-    private:
-        Node *root;
-    public:
-        BST(){
-            root = NULL;
-        }
-        bool is_empty(){
-            return root == NULL;
-        }
-        void insert(int val){
-            if( is_empty() )
-                root = new Node(NULL, val);
-            else{
-                Node *current = root, *parent;
-                while( current != NULL ){
-                    parent = current;
-                    current = ( val <= current->value ) ? current->left : current->right;
-                }
+private:
+    Node<DataType> *root;
 
-                if( val <= parent->value )
-                    parent->left = new Node(parent, val);
-                else
-                    parent->right = new Node(parent, val);
-            }
-        }
+public:
+    BST() {
+        root = NULL;
+    }
 
-        void preorder(Node *current = NULL){
-            if( current == NULL ) current = root;
+    bool is_empty() {
+        return root == NULL;
+    }
 
-            // Pre-Order = (Root, Left, Right)
-            if(current != NULL){
-                cout<<current->value <<", ";
-                if( current->left != NULL ) preorder(current->left);
-                if( current->right != NULL ) preorder(current->right);
-            }
-        }
+    void insert(DataType val) {
 
-        void inorder(Node *current = NULL){
-            if( current == NULL ) current = root;
-
-            // In-Order = (Left, Root, Right)
-            if( current != NULL ){
-                if( current->left != NULL ) inorder(current->left);
-                cout<<current->value <<", ";
-                if( current->right != NULL ) inorder(current->right);
-            }
-        }
-
-        void postorder(Node *current = NULL){
-            if( current == NULL ) current = root;
-
-            // Post-Order = (Left, Right, Root)
-            if( current != NULL ){
-                if( current->left != NULL ) postorder(current->left);
-                if( current->right != NULL ) postorder(current->right);
-                cout<<current->value <<", ";
-            }
-        }
-
-        void levelorder(Node *current = NULL){
-
-            vector<Node*> next_level;
-
-            if( current == NULL and root != NULL ){
-                next_level.push_back(root);
+        if( is_empty() )
+            root = new Node<DataType>(NULL, val);
+        else {
+            Node<DataType> *current = root, *parent;
+            while( current != NULL ) {
+                parent = current;
+                current = ( val <= current->value ) ? current->left : current->right;
             }
 
-            while( next_level.size() > 0 ){
-                cout <<next_level[0]->value <<", ";
-
-                if( next_level[0]->left != NULL ){
-                    next_level.push_back(next_level[0]->left);
-                }
-                if( next_level[0]->right != NULL ){
-                    next_level.push_back(next_level[0]->right);
-                }
-
-                next_level.erase(next_level.begin());
-            }
-        }
-
-        Node *search(int find){
-            Node *current = root;
-
-            while( current != NULL and current->value != find )
-                current = (current->value < find) ? current->right : current->left;
-
-            return current;
-        }
-
-        Node *maximum(Node *current = NULL){
-            if( current == NULL ) current = root;
-
-            while( current != NULL and current->right != NULL ) current = current->right;
-
-            return current;
-        }
-
-        Node *minimum(Node *current = NULL){
-            if( current == NULL ) current = root;
-
-            while( current != NULL and current->left != NULL ) current = current->left;
-
-            return current;
-        }
-
-        Node *successor(Node *current){
-
-            if( current == NULL ) return NULL;
-
-            if( current->right == NULL ){
-                while( current->parent != NULL and current->parent->right == current )
-                    current = current->parent;
-                return current->parent;
-            }
+            if( val <= parent->value )
+                parent->left = new Node<DataType>(parent, val);
             else
-                return minimum(current->right);
+                parent->right = new Node<DataType>(parent, val);
+        }
+    }
+
+    void preorder(Node<DataType> *current = NULL){
+        if( current == NULL ) current = root;
+
+        // Pre-Order = (Root, Left, Right)
+        if(current != NULL){
+            cout<<current->value <<", ";
+            if( current->left != NULL ) preorder(current->left);
+            if( current->right != NULL ) preorder(current->right);
+        }
+    }
+
+    void inorder(Node<DataType> *current = NULL){
+        if( current == NULL ) current = root;
+
+        // In-Order = (Left, Root, Right)
+        if( current != NULL ){
+            if( current->left != NULL ) inorder(current->left);
+            cout<<current->value <<", ";
+            if( current->right != NULL ) inorder(current->right);
+        }
+    }
+
+    void postorder(Node<DataType> *current = NULL){
+        if( current == NULL ) current = root;
+
+        // Post-Order = (Left, Right, Root)
+        if( current != NULL ){
+            if( current->left != NULL ) postorder(current->left);
+            if( current->right != NULL ) postorder(current->right);
+            cout<<current->value <<", ";
+        }
+    }
+
+    void levelorder(Node<DataType> *current = NULL){
+
+        vector<Node<DataType>*> next_level;
+
+        if( current == NULL and root != NULL ){
+            next_level.push_back(root);
         }
 
-        Node *predecessor(Node *current){
+        while( next_level.size() > 0 ){
+            cout <<next_level[0]->value <<", ";
 
-            if( current == NULL ) return NULL;
-
-            if( current->left == NULL ){
-                while( current->parent != NULL and current->parent->left == current )
-                    current = current->parent;
-                return current->parent;
+            if( next_level[0]->left != NULL ){
+                next_level.push_back(next_level[0]->left);
             }
-            else
-                return maximum(current->left);
+            if( next_level[0]->right != NULL ){
+                next_level.push_back(next_level[0]->right);
+            }
+
+            next_level.erase(next_level.begin());
         }
+    }
+
+    Node<DataType> *search(DataType find){
+        Node<DataType> *current = root;
+
+        while( current != NULL and current->value != find )
+            current = (current->value < find) ? current->right : current->left;
+
+        return current;
+    }
+
+    Node<DataType> *maximum(Node<DataType> *current = NULL){
+        if( current == NULL ) current = root;
+
+        while( current != NULL and current->right != NULL ) current = current->right;
+
+        return current;
+    }
+
+    Node<DataType> *minimum(Node<DataType> *current = NULL){
+        if( current == NULL ) current = root;
+
+        while( current != NULL and current->left != NULL ) current = current->left;
+
+        return current;
+    }
+
+    Node<DataType> *successor(Node<DataType> *current){
+
+        if( current == NULL ) return NULL;
+
+        if( current->right == NULL ){
+            while( current->parent != NULL and current->parent->right == current )
+                current = current->parent;
+            return current->parent;
+        }
+        else
+            return minimum(current->right);
+    }
+
+    Node<DataType> *predecessor(Node<DataType> *current){
+
+        if( current == NULL ) return NULL;
+
+        if( current->left == NULL ){
+            while( current->parent != NULL and current->parent->left == current )
+                current = current->parent;
+            return current->parent;
+        }
+        else
+            return maximum(current->left);
+    }
 };
 
 int main(){
 
     int ins_values[] = {28, 33, 21, 37, 21, 47, 40, 22, 1, 16};
-    BST a;
+    BST<int> a;
 
     for(int i=0; i<10; i++)
         a.insert(ins_values[i]);
@@ -181,7 +189,7 @@ int main(){
     a.levelorder();
 
     int search = 33;
-    Node *srh = a.search(search);
+    Node<int> *srh = a.search(search);
     if( ! srh )
         cout <<"\n\n" <<search <<" not found in tree";
     else
@@ -190,7 +198,7 @@ int main(){
     cout<<"\n\nMax node = " <<a.maximum()->value;
     cout<<"\nMin node = " <<a.minimum()->value;
 
-    Node *temp = a.successor(a.search(22));
+    Node<int> *temp = a.successor(a.search(22));
     if( temp != NULL ) cout<<"\n\nSuccessor of 47 = " <<temp->value;
     temp = a.predecessor(a.search(22));
     if( temp != NULL ) cout<<"\nPredecessor of 47 = " <<temp->value;

092b7cbd76cf216f965d27401f0eae6525e60a73
ashwek <ashwekswamy@protonmail.com>
2020-04-22 15:05:42 +0530

    use template class for Stack.cpp

--- 3_Data_Structures/10_Elementary_Data_Structures/Stack.cpp
+++ 3_Data_Structures/10_Elementary_Data_Structures/Stack.cpp
@@ -1,78 +1,88 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
+template<class DataType>
 class Stack{
 
-	private:
-		int *data, top, size;
+private:
+    DataType *data;
+    int top, size;
 
-	public:
+public:
 
-		Stack(int s = 10){
-			data = new int[s];
-			top = -1;
-			size = s;
-		}
+    Stack(int s = 10) {
+        data = new DataType[s];
+        top = -1;
+        size = s;
+    }
 
-		~Stack(){
-			free(data);
-		}
+    ~Stack() {
+        free(data);
+    }
 
-		int is_empty(){
-			return (top == -1);
-		}
+    int is_empty() {
+        return (top == -1);
+    }
 
-		int is_full(){
-			return (top == size-1);
-		}
+    int is_full() {
+        return (top == size-1);
+    }
 
-		int push(int value){
-			if( is_full() )
-				return 0;
-			data[++top] = value;
-			return 1;
-		}
+    int push(DataType value) {
+        if( is_full() )
+            return 0;
+        data[++top] = value;
+        return 1;
+    }
 
-		int pop(){
-			if( is_empty() )
-				return -1;
-			return data[top--];
-		}
+    int pop(){
 
-		void display(){
-			if( ! is_empty() ){
-				for(int i=top; i>=0; i--) cout<<data[i] <<", ";
-			}
-		}
+        if( is_empty() )
+            return -1;
+        return data[top--];
+
+    }
+
+    void display(){
+        if( ! is_empty() ){
+            for(int i=top; i>=0; i--)
+                cout<<data[i] <<", ";
+        }
+    }
+
 };
 
 
 int main(){
 
-	Stack S1 = Stack(5);
-	int i = 1;
-	while( ! S1.is_full() ){
-		cout<<"Push " <<i <<endl;
-		S1.push(i);
-		i += 5;
-	}
+    srand(time(NULL));
 
-	cout<<"\n\nStack : ";
-	S1.display();
+    Stack<int> S1 = Stack<int>(5);
+    int temp;
+    cout <<"\nPushing random values until stack is full:\n";
+    while( ! S1.is_full() ) {
+        temp = random() % 100 + 1;
+        cout<<"Push : " <<temp <<endl;
+        S1.push(temp);
+    }
 
-	cout<<"\n\nPop, popped value = " <<S1.pop();
+    cout<<"\nStack : ";
+    S1.display();
 
-	cout<<"\n\nStack : ";
-	S1.display();
-	cout<<endl;
+    cout<<"\n\nPop: popped value = " <<S1.pop();
 
-	while( ! S1.is_empty() ){
-		cout<<"Popped value " <<S1.pop() <<endl;
-	}
+    cout<<"\n\nStack : ";
+    S1.display();
 
-	cout<<endl;
+    cout<<"\n\nPop stack until it is empty :";
+    while( ! S1.is_empty() ){
+        cout<<"\nPopped value : " <<S1.pop();
+    }
 
-	return 0;
+    cout<<endl;
+
+    return 0;
 }

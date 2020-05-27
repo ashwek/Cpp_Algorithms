f53ed427f31873436212e5d7967352e0c71b34e6
ashwek <ashwekswamy@protonmail.com>
2020-05-27 18:39:57 +0530

    Update DFS

--- 6_Graph_Algorithms/22_Elementary_Graph_Algorithms/DFS.cpp
+++ 6_Graph_Algorithms/22_Elementary_Graph_Algorithms/DFS.cpp
@@ -1,161 +1,111 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
-using namespace std;
+enum colors {WHITE, GRAY, BLACK};
 
-class graph{
-public:
-    int v, count;
-    vector<vector<int>> adj;
-    vector<int> distance, parent, state, timestamp[2];
-    graph(int nodes);
-    void add_edge(int src, int dst);
-    void print_adj();
-    void dfs();
-    void dfs_visit(int src);
-    int print_path(int src, int dst);
+class Vertex {
+ public:
+    int value;
+    colors vertexColor;
+    Vertex* parent;
+    int discoverTimestamp;
+    int finishedTimestamp;
+
+    explicit Vertex(int value) {
+        this->value = value;
+    }
 };
 
-graph::graph(int nodes = 0){
-    v = nodes;
-    adj.resize(nodes);
-    distance.resize(nodes);
-    parent.resize(nodes);
-    state.resize(nodes);
-    timestamp[0].resize(nodes);
-    timestamp[1].resize(nodes);
+class Graph {
+ public:
+    int time;
+    std::map<Vertex*, std::vector<Vertex*>> edges;
+    Graph();
+    void add_edge(Vertex *src, Vertex *dst);
+    void print_adj();
+    void dfs();
+    void dfs_visit(Vertex *src);
+    int print_path(Vertex *src, Vertex *dst);
+};
+
+Graph::Graph() {
 }
 
-void graph::add_edge(int src, int dst){
-    adj[src].push_back(dst);
+void Graph::add_edge(Vertex *src, Vertex *dst) {
+    if ( edges.count(src) == 0 )
+        edges[src] = std::vector<Vertex*>();
+    edges[src].push_back(dst);
 }
 
-void graph::print_adj(){
-    for( int i = 0; i < v; i++){
-        cout<<i <<" -> ";
-        for( int j = 0; j < adj[i].size(); j++)
-            cout<<adj[i][j] <<", ";
-        cout<<endl;
+void Graph::print_adj() {
+    for (auto it : edges) {
+        std::cout <<it.first->value <<" -> ";
+        for (Vertex *temp : it.second) {
+            std::cout <<temp->value <<", ";
+        }
+        std::cout <<std::endl;
     }
 }
 
-void pr_vec(vector<int> queue){
-    for(int i = 0; i<queue.size(); i++)
-        cout<<i <<" -> " <<queue[i] <<endl;
+void Graph::dfs_visit(Vertex *src) {
+    time++;
+    src->discoverTimestamp = time;
+    src->vertexColor = GRAY;
+
+    for (Vertex *temp : edges[src]) {
+        if (temp->vertexColor == WHITE) {
+            temp->parent = src;
+            std::cout <<temp->value <<" -> ";
+            dfs_visit(temp);
+        }
+    }
+
+    time++;
+    src->finishedTimestamp = time;
+    src->vertexColor = BLACK;
 }
 
-void graph::dfs_visit(int src){
+void Graph::dfs() {
+    for (auto it : edges) {
+        it.first->vertexColor = WHITE;
+        it.first->parent = NULL;
+    }
+    time = 0;
 
-    for(int i = 0; i < adj[src].size(); i++){
-        if( state[adj[src][i]] == 0 ){
-            state[adj[src][i]] = 1;
-            cout<<adj[src][i] <<" -> ";
-            distance[adj[src][i]] = distance[src]+1;
-            parent[adj[src][i]] = src;
-            timestamp[0][adj[src][i]] = count++;
-            dfs_visit(adj[src][i]);
-            timestamp[1][adj[src][i]] = count++;
+    for (auto it : edges) {
+        if (it.first->vertexColor == WHITE) {
+            std::cout <<it.first->value <<" -> ";
+            dfs_visit(it.first);
+            std::cout <<std::endl;
         }
     }
 }
 
-void graph::dfs(){
+int main() {
+    Graph g;
+    std::vector<Vertex*> vertices(6);
 
-    for(int i=0; i<v; i++){
-        state[i] = 0;
-        parent[i] = distance[i] = timestamp[0][i] = timestamp[1][i] = -1;
+    for (int i = 0; i < vertices.size(); i++) {
+        vertices[i] = new Vertex(i);
     }
 
-    count = 1;
-    for(int i = 0; i < v; i++){
-        if( state[i] == 0){
-            state[i] = 1;
-            timestamp[0][i] = count++;
-            cout <<i <<" -> ";
-            dfs_visit(i);
-            timestamp[1][i] = count++;
-            cout<<endl;
-        }
-    }
-}
+    g.add_edge(vertices[0], vertices[1]);
+    g.add_edge(vertices[0], vertices[3]);
+    g.add_edge(vertices[1], vertices[4]);
+    g.add_edge(vertices[2], vertices[5]);
+    g.add_edge(vertices[2], vertices[4]);
+    g.add_edge(vertices[3], vertices[1]);
+    g.add_edge(vertices[4], vertices[3]);
+    g.add_edge(vertices[5], vertices[5]);
 
-int graph::print_path(int src, int dst){
-    if( dst == src )
-        cout<<src<<", ";
-    else if( parent[dst] == -1 )
-        return 0;
-    else if(print_path(src, parent[dst] ))
-        cout<<dst <<", ";
-    else
-        return 0;
-    return 1;
-}
 
-int main(){
+    std::cout <<"\nAdjacency list :\n";
+    g.print_adj();
 
-    graph g(6);
-
-    /*g.add_edge(0, 1);
-    g.add_edge(0, 2);
-    g.add_edge(1, 0);
-    g.add_edge(2, 0);
-    g.add_edge(2, 3);
-    g.add_edge(3, 2);
-    g.add_edge(3, 4);
-    g.add_edge(3, 5);
-    g.add_edge(4, 3);
-    g.add_edge(4, 5);
-    g.add_edge(4, 6);
-    g.add_edge(5, 3);
-    g.add_edge(5, 4);
-    g.add_edge(5, 6);
-    g.add_edge(5, 7);
-    g.add_edge(6, 4);
-    g.add_edge(6, 5);
-    g.add_edge(6, 7);
-    g.add_edge(7, 5);
-    g.add_edge(7, 6);*/
-
-    g.add_edge(0, 1);
-    g.add_edge(0, 2);
-    g.add_edge(1, 2);
-    g.add_edge(2, 3);
-    g.add_edge(3, 1);
-    g.add_edge(4, 3);
-    g.add_edge(4, 5);
-    g.add_edge(5, 5);
-
-    /*g.add_edge(1, 0);
-    g.add_edge(2, 0);
-    g.add_edge(2, 3);
-    g.add_edge(3, 5);
-    g.add_edge(4, 3);
-    g.add_edge(4, 6);
-    g.add_edge(5, 4);
-    g.add_edge(5, 6);
-    g.add_edge(5, 7);
-    g.add_edge(6, 7);
-    g.add_edge(7, 5);*/
-
-    //g.print_adj();
-
-    cout<<"\nDFS traversal : ";
+    std::cout <<"\nDFS traversal :\n";
     g.dfs();
 
-    cout<<"\nParent :\n";
-    pr_vec(g.parent);
-
-    cout<<"\nDistance :\n";
-    pr_vec(g.distance);
-
-    cout<<"\nPrev Time :\n";
-    pr_vec(g.timestamp[0]);
-
-    cout<<"\nPost Time :\n";
-    pr_vec(g.timestamp[1]);
-
-    g.print_path(1, 0);
-
-    cout<<endl;
+    std::cout <<std::endl;
     return 0;
 }
